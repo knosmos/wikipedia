d5d4502dc80113a219ec727d39c3fc521e05939a
knosmos <30610197+knosmos@users.noreply.github.com>
2021-03-29 08:55:13 -0400

    Reduce flickering, rearrange functions

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 A command line interface for reading articles on wikipedia.
 '''
 
+''' IMPORTS '''
+
 import os # used to find terminal dimensions
 import sys # used to update terminal
 import keyboard # used to handle keyboard input
@@ -14,6 +16,8 @@ if os.name == "nt":
     import ctypes # for added pain (and to move the cursor)
     from ctypes import c_long, c_wchar_p, c_ulong, c_void_p
     cursor_move = True
+
+''' PRIMITIVE GRAPHICS '''
 
 # Terminal colors
 class colors:
@@ -51,6 +55,8 @@ def erase(x1,x2,y1,y2): # Erases a specific rectangle of the window
         for y in range(y1,y2+1):
             write(x,y," ")
 
+''' DRAW WINDOW '''
+
 if cursor_move:
     gHandle = ctypes.windll.kernel32.GetStdHandle(c_long(-11))
 sx, sy = (0,0)
@@ -66,26 +72,20 @@ pad_b = 0
 pad_l = 0
 pad_r = 0
 
-def keyInput(keyObj):
-    global query_str, offset
-    key = keyObj.name # get unicode representation of keyObj
-    if key == "space": key = " "
-    if key == "backspace": query_str = query_str[:-1]
-    if len(key) == 1: # check if it is actually a character and not something like [shift]
-        query_str += key
-    if key == "enter":
-        if len(query_str): # Check if there is actually a query
-            getPage() # Send request to wikipedia
-            query_str = ""
-    if key == "up":
-        offset = max(0,offset-1)
-        writePage()
-    if key == "down":
-        offset += 1
-        writePage()
-    drawQuery()
-
-keyboard.on_press(keyInput)
+def drawSplash():
+    clear()
+    title = [
+        "_       ___ __   _                ___       ",
+        "| |     / (_) /__(_)___  ___  ____/ (_)___ _",
+        "| | /| / / / //_/ / __ \/ _ \/ __  / / __ `/",
+        "| |/ |/ / / ,< / / /_/ /  __/ /_/ / / /_/ / ",
+        "|__/|__/_/_/|_/_/ .___/\___/\__,_/_/\__,_/  ",
+        "               /_/                          "
+    ]
+    for i in range(len(title)):
+        write(0,1+i,title[i],colors.OKGREEN)
+        sys.stdout.flush()
+        time.sleep(0.05)
 
 def drawWindow(sx,sy):
     global query_str
@@ -117,8 +117,44 @@ def drawQuery():
         ctypes.windll.kernel32.SetConsoleCursorPosition(gHandle, c_ulong(value))
     sys.stdout.flush()
 
+def redraw():
+    global page, rawpage
+    drawWindow(sx,sy)
+    drawQuery()
+    page = makePage(rawpage)
+    writePage()
+
+''' QUERY '''
+
+def keyInput(keyObj):
+    global query_str, offset
+    key = keyObj.name # get unicode representation of keyObj
+    if key == "space": key = " "
+    if key == "backspace": query_str = query_str[:-1]
+    if len(key) == 1: # check if it is actually a character and not something like [shift]
+        query_str += key
+    if key == "enter":
+        if len(query_str): # Check if there is actually a query
+            getPage() # Send request to wikipedia
+            query_str = ""
+    if key == "up":
+        offset = max(0,offset-1)
+        writePage()
+    if key == "down":
+        offset += 1
+        writePage()
+    drawQuery()
+
+keyboard.on_press(keyInput)
+
 def getPage():
     global query_str, page, rawpage, offset
+    # Loading screen
+    offset = 0
+    page = makePage("Loading...")
+    writePage()
+
+    # Send query
     try:
         rawpage = wikipedia.page(query_str,auto_suggest=False)
         rawpage = colors.WARNING+rawpage.title.upper()+colors.ENDC+"\n"+rawpage.content # title (in yellow) and content
@@ -126,6 +162,7 @@ def getPage():
         rawpage = colors.WARNING+query_str+" may refer to:"+colors.ENDC+"\n"+"\n".join(e.options)
     except wikipedia.exceptions.PageError:
         rawpage = "No pages found for '%s'. Try a different query." %(query_str)
+    
     # Add color to headings
     rawpage = rawpage.replace(" ==="," ==="+colors.ENDC)
     rawpage = rawpage.replace("=== ",colors.FAIL+"=== ")     
@@ -133,9 +170,12 @@ def getPage():
     rawpage = rawpage.replace("== ",colors.FAIL+"== ")
     
     # Textwrap lines
+    erase(2+pad_l,sx-1-pad_r,4+pad_t,sy-1)
     page = makePage(rawpage)
     offset = 0
     redraw()
+
+''' DRAW PAGE '''
 
 def makePage(page):
     w = textwrap.TextWrapper(width=sx-2-pad_l-pad_r,replace_whitespace=False)
@@ -152,55 +192,42 @@ def makePage(page):
 
 def writePage():
     global page, sx, offset
-    erase(2+pad_l,sx-1-pad_r,4+pad_t,sy-1)
+    linew = sx-pad_r-pad_l-2 # Width of the drawing area
     for i in range(offset,min(len(page),sy-4+offset-pad_t)):
-        write(2+pad_l,4+i-offset+pad_t,page[i])
+        # Instead of clearing the canvas and redrawing, we add spaces
+        # to the end of each line to clear out any text that might have
+        # been there before. This significantly reduces flickering.
+        write(2+pad_l,4+i-offset+pad_t,page[i]+" "*(linew-len(page[i])))
+    erase(2+pad_l,sx-1-pad_r,4+pad_t+len(page)-offset,sy-1)
     sys.stdout.flush()
 
-def redraw():
-    global page, rawpage
-    drawWindow(sx,sy)
+''' MAIN '''
+def main():
+    global sx, sy, pad_r, pad_l
+    drawSplash()
+    time.sleep(0.1)
     drawQuery()
-    page = makePage(rawpage)
-    writePage()
+    try:
+        while True:
+            # Get dimensions of terminal window
+            s = os.get_terminal_size()
+            nsy = s.lines
+            nsx = s.columns
+            # If window has been resized, redraw the window
+            if nsy != sy or nsx != sx:
+                sx, sy = nsx, nsy
+                # set left/right padding to 10% if window is large enough
+                if sx > 60:
+                    pad_r = int(sx/10)
+                    pad_l = int(sx/10)
+                else:
+                    pad_r = 1
+                    pad_l = 1
+                redraw()
+            time.sleep(0.05)
+    except KeyboardInterrupt:
+        clear() # Clear the screen before we exit
+        sys.exit()
 
-def drawSplash():
-    clear()
-    title = [
-        "_       ___ __   _                ___       ",
-        "| |     / (_) /__(_)___  ___  ____/ (_)___ _",
-        "| | /| / / / //_/ / __ \/ _ \/ __  / / __ `/",
-        "| |/ |/ / / ,< / / /_/ /  __/ /_/ / / /_/ / ",
-        "|__/|__/_/_/|_/_/ .___/\___/\__,_/_/\__,_/  ",
-        "               /_/                          "
-    ]
-    for i in range(len(title)):
-        write(0,1+i,title[i],colors.OKGREEN)
-        sys.stdout.flush()
-        time.sleep(0.05)
-
-# Splash screen, for fun
-drawSplash()
-time.sleep(0.1)
-drawQuery()
-try:
-    while True:
-        # Get dimensions of terminal window
-        s = os.get_terminal_size()
-        nsy = s.lines
-        nsx = s.columns
-        # If window has been resized, redraw the window
-        if nsy != sy or nsx != sx:
-            sx, sy = nsx, nsy
-            # set left/right padding to 10% if window is large enough
-            if sx > 60:
-                pad_r = int(sx/10)
-                pad_l = int(sx/10)
-            else:
-                pad_r = 1
-                pad_l = 1
-            redraw()
-        time.sleep(0.05)
-except KeyboardInterrupt:
-    clear() # Clear the screen before we exit
-    sys.exit()
+if __name__ == '__main__':
+    main()
